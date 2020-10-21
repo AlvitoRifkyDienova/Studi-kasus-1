818ab6de1d3c958349156b40c10f2d780e764766
unknown <alvito_rifky_29rpl@student.smktelkom-mlg.sch.id>
2020-10-21 12:28:44 +0700

    berhasil

---
+++ src/studi/kasus/kartupelajar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studi.kasus;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class kartupelajar {
+    public static void main (String[] args){
+        String nama,ttl,jeniskelamin,agama,alamat;
+        int nis;
+        nama= "Boby";
+        nis= 9089;
+        ttl= "Malang, 16 Agustus 2000";
+        jeniskelamin= "Laki-laki";
+        agama= "Islam";
+        alamat= "Jl.Rajawali 3";
+        
+        System.out.println(" KARTU PELAJAR ");
+        System.out.println("Nama: "+nama);
+        System.out.println("NIS: "+nis);
+        System.out.println("Tempat tanggal lahir: "+ttl);
+        System.out.println("Jenis kelamin: "+jeniskelamin);
+        System.out.println("Agam: "+agama);
+        System.out.println("Alamat: "+alamat);
+        
+    
+}}
